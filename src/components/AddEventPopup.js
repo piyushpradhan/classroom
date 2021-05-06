d86ef6fc7d52a733c9204cc66da10168ffa3c10c
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-05-06 12:15:42 +0530

    fixed issues with responsive layouts

---
+++ src/components/AddEventPopup.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function AddEventPopup() {
+  return (
+    <div className="relative md:w-96 h-full">
+      <div className="absolute w-full h-full bg-grey-900 -right-2 -bottom-2"></div>
+      <div className="relative w-full border-2 border-black bg-white p-8 flex flex-col items-start">
+        <div className="text-xl font-bold">Add Event</div>
+        <div className="mt-4">
+          <input
+            className="focus:outline-none text-sm"
+            type="text"
+            placeholder="Enter event Title"
+          />
+        </div>
+        <div className="flex flex-row justify-between space-x-14">
+          <div className="flex flex-col text-sm font-bold mt-4">
+            <label>Start</label>
+            <input className="" type="time" placeholder="Start" />
+          </div>
+          <div className="flex flex-col text-sm font-bold mt-4">
+            <label>End</label>
+            <input className="" type="time" placeholder="Start" />
+          </div>
+        </div>
+        <div className="mt-4 w-full">
+          <textarea
+            className="text-sm w-full w-max-full focus:outline-none"
+            placeholder="Add event description..."
+          ></textarea>
+        </div>
+        <div className="flex flex-row mt-2 w-full justify-end">
+          <button className="bg-grey-900 px-4 py-2 text-white font-bold outline-none border-none focus:outline-none">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddEventPopup;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import Modal from "react-modal";
+
+import { BiCalendarPlus } from "react-icons/bi";
+
 import Calendar from "../components/Calendar";
 import AttendanceChart from "../components/AttendanceChart";
 import DashboardHeader from "../components/DashboardHeader";
@@ -10,10 +14,27 @@ import { LABEL } from "../constants/actionTypes";
 import { AuthProvider } from "../context/AuthContext";
 import { useDashboardContext } from "../context/DashboardContext";
 import ScheduledEvent from "./ScheduledEvent";
+import AddEventPopup from "./AddEventPopup";
+
+const modalStyle = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    position: "absolute",
+    border: "none",
+    background: "transparent",
+    borderRadius: "0px",
+  },
+};
 
 function Dashboard() {
   const currentUser = firebase.auth().currentUser;
   const [newSubjectName, setNewSubjectName] = useState("");
+  const [modalIsOpen, toggleModal] = useState(false);
 
   const {
     state: { labels },
@@ -29,14 +50,29 @@ function Dashboard() {
     });
   }
 
+  function openModal() {
+    toggleModal(true);
+  }
+
+  function closeModal() {
+    toggleModal(false);
+  }
+
   return (
     <AuthProvider>
-      <div className="w-screen h-screen xl:grid xl:grid-cols-7 lg:flex lg:flex-col">
-        <div className="xl:col-span-5 lg:col-span-7 bg-white flex flex-col px-16">
+      <div className="w-screen h-screen flex 2xl:flex-row flex-col justify-between">
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={modalStyle}
+        >
+          <AddEventPopup />
+        </Modal>
+        <div className="flex-grow bg-white flex flex-col px-16 max-h-full">
           <DashboardHeader />
           <div className="text-xl my-4 font-bold">Attendance Chart</div>
-          <div className="flex flex-col items-center sm:flex-row border-2 border-grey-900 rounded-md xl:w-full lg:w-5/6 h-2/5 py-4">
-            <div className="sm:w-3/5 sm:h-full my-10 mx-4">
+          <div className="flex flex-col items-center xl:h-2/5 sm:flex-row border-2 border-grey-900 rounded-md xl:w-full lg:w-5/6 py-4">
+            <div className="sm:w-3/5 sm:h-full mx-4">
               <AttendanceChart />
             </div>
             <div className="w-full sm:w-2/5 m-4 flex flex-col">
@@ -55,7 +91,7 @@ function Dashboard() {
                   );
                 })}
               </div>
-              <div className="flex flex-col border border-grey-900 rounded-md mt-11 mx-8 sm:ml-8">
+              <div className="flex flex-col border border-grey-900 rounded-md sm:mt-11 mx-8 sm:ml-8">
                 <div className="text-md font-semibold text-center mt-2">
                   Add new subject
                 </div>
@@ -66,7 +102,10 @@ function Dashboard() {
                   value={newSubjectName}
                   onChange={(e) => setNewSubjectName(e.target.value)}
                 />
-                <button className="font-bold uppercase text-white bg-grey-900">
+                <button
+                  onClick={addSubject}
+                  className="font-bold uppercase text-white bg-grey-900"
+                >
                   Add
                 </button>
               </div>
@@ -74,9 +113,18 @@ function Dashboard() {
           </div>
           <TodoComponent />
         </div>
-        <div className="flex flex-col items-center md:mt-0 md:px-2 mt-8 col-span-2 bg-grey-200">
+        <div className="flex flex-col items-center md:mt-0 md:px-2 mt-8 flex-grow-1 bg-grey-200">
           <Calendar />
-          <div className="w-full px-4 mt-10">
+          <div className="flex flex-row md:px-4 mt-10 w-full justify-between">
+            <div className="font-bold text-lg">Scheduled Events</div>
+            <button
+              classNmae="outline-none border-none focus:outline-none focus:border-none"
+              onClick={openModal}
+            >
+              <BiCalendarPlus className="text-2xl focus:outline-none" />
+            </button>
+          </div>
+          <div className="w-full px-4 mt-2">
             <ScheduledEvent />
             <ScheduledEvent />
             <ScheduledEvent />
