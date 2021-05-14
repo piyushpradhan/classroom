10e6728aeeb26421ca683c71c9ea0eade85e54a0
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-05-14 23:36:04 +0530

    configured the routes to display login route at /

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,19 +2,17 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 import { useAuthContext } from "../context/AuthContext";
+import firebase from '../firebase/firebase'; 
 
 export default function PrivateRoute({ component: Component, ...rest }) {
-  const {
-    state: { user },
-  } = useAuthContext();
-
-  console.log(user);
+  const { currentUser } = useAuthContext();
+  console.log(firebase.auth().isAuthenticated); 
 
   return (
     <Route
       {...rest}
       render={(props) => {
-        user ? <Component {...props} /> : <Redirect to="/login" />;
+        firebase.auth().isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />;
       }}
     ></Route>
   );
