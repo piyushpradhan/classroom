4b86992033b58b37b0471351bfb86b1a553f8937
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-05-14 23:13:33 +0530

    added craco to package.json

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,21 +1,21 @@
-import React from 'react'; 
-import { Route, Redirect } from 'react-router-dom'; 
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-import { useAuthContext } from '../context/AuthContext'; 
+import { useAuthContext } from "../context/AuthContext";
 
 export default function PrivateRoute({ component: Component, ...rest }) {
-  const { 
-    state: { user }, 
-  } = useAuthContext(); 
-  
-  console.log(user); 
+  const {
+    state: { user },
+  } = useAuthContext();
+
+  console.log(user);
 
   return (
     <Route
       {...rest}
-      render = {props => {
-        user ? <Component {...props} /> : <Redirect to="/login"/>
+      render={(props) => {
+        user ? <Component {...props} /> : <Redirect to="/login" />;
       }}
-      ></Route>
-  ); 
+    ></Route>
+  );
 }
