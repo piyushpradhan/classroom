02094095f8f00f33acabff624384c6f3c69e9dcd
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2021-05-17 10:02:43 +0530

    modified login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import firebase from "../firebase/firebase";
 import { useAuthContext } from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
+
+import "../public/css/login.css";
+
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -35,18 +38,6 @@ function LoginForm() {
     });
   }
 
-  const login = async (e) => {
-    e.preventDefault();
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((response) => {
-        console.log(response.user);
-        setCurrentUser(response.user);
-        history.push("/dashboard");
-      });
-  };
-
   const loginWithGoogle = async (e) => {
     e.preventDefault();
     firebase
@@ -67,79 +58,33 @@ function LoginForm() {
           .catch((err) => {
             var code = err.code;
             var errorMessage = err.message;
-            console.log(code + errorMessage); 
+            console.log(code + errorMessage);
           });
       });
   };
 
   return (
-    <div className="flex flex-col h-screen justify-center">
-      <form
-        onSubmit={login}
-        className="bg-white form rounded-xl border border-black px-8 pt-6 pb-10 w-5/12 max-w-lg min-w-min self-center"
-      >
+    <div className="parent-login-container flex flex-col h-screen w-screen justify-center items-center">
+      <div className="w-screen h-screen bg-white opacity-70 absolute z=0"></div>
+      <div className="bg-white rounded-xl border-2 border-black p-8 flex flex-col z-10">
         <p className="font-bold text-4xl text-center mb-2">Classroom</p>
         <p className="text-md text-center mb-4">Please login to continue.</p>
-        <div className="flex flex-col text-sm">
-          <div className="flex flex-row justify-start">
-            <label className="text-lg font-bold mb-2">Email</label>
-          </div>
-          <input
-            className="border border-black rounded-md p-3 mb-4 focus:outline-none"
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
+        <button
+          onClick={loginWithGoogle}
+          className="flex flex-row max-w-max border border-black rounded rounded-full justify-start items-center mt-6 focus:outline-none"
+        >
+          <img
+            className="m-1 max-w-max"
+            src="/images/google-login.png"
+            alt="Google Logo"
           />
-
-          <div className="flex flex-row justify-start">
-            <label className="text-lg font-bold mb-2">Password</label>
-          </div>
-          <input
-            className="border border-black rounded-md p-3 mb-6 focus:outline-none"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-          />
-
-          <div className="relative w-full" role="button">
-            <div className="absolute bg-grey-900 w-full h-full -right-1 -bottom-1"></div>
-            <div className="relative bg-white space-y-7">
-              <input
-                type="submit"
-                value="Login"
-                className="w-full items-center justify-center uppercase border border-black p-2 text-md font-bold"
-              />
-            </div>
-          </div>
-
-          <div className="flex flex-row w-full bg-blue align-center mt-6">
-            <div className="bg-white p-0 m-1">
-              <img
-                src="/images/google_logo.png"
-                height={36}
-                width={36}
-                alt="Google Logo"
-              />
-            </div>
-            <button
-              type="button"
-              value="Sign in with Google"
-              onClick={loginWithGoogle}
-              className="w-full bg-blue text-md font-bold text-white focus:outline-none"
-            >
+          <div className="flex-grow-1 flex flex=row justify-center">
+            <div className="w-full px-4 text-md font-bold focus:outline-none">
               Continue with Google
-            </button>
+            </div>
           </div>
-          {/* <div className="text-grey-900 text-md text-center font-semibold mt-2">
-            Don't have an account?{" "}
-            <a href="/auth/register">
-              <span className="text-blue">Create one!</span>
-            </a>
-          </div> */}
-        </div>
-      </form>
+        </button>
+      </div>
     </div>
   );
 }
